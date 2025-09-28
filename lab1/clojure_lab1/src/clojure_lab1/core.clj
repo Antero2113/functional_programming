3ab3cddef8ff059811ee51c550dd16aeae720713
Antero2113 <antero3112@gmail.com>
2025-09-28 22:00:04 +0300

    Первый вариант реализации решения задачи №5

--- lab1/clojure_lab1/src/clojure_lab1/core.clj
+++ lab1/clojure_lab1/src/clojure_lab1/core.clj
@@ -1,7 +1,24 @@
 (ns clojure-lab1.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn gcd [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(defn lcm [a b]
+  (if (or (zero? a) (zero? b))
+    0
+    (/ (Math/abs (* a b)) (gcd a b))))
+
+(defn smallest-multiple [n]
+  (reduce lcm (range 1 (inc n))))
+
+(defn -main [& args]
+  (let [result (smallest-multiple 20)]
+    (println "Result:" result)
+    
+    ;; Простая проверка
+    (println "Verification passed:" 
+             (every? #(zero? (mod result %)) (range 1 21)))))

01f56b0d64b79a8f0329f898b15a83c18fe749f8
Antero2113 <antero3112@gmail.com>
2025-12-23 05:38:43 +0300

    Updated lab3: simplify realization

--- lab3/interpolation/src/interpolation/core.clj
+++ lab3/interpolation/src/interpolation/core.clj
@@ -2,276 +2,156 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
+;; Парсинг и валидация входных данных
+
 (defn parse-line [s]
-  "Парсит строку с координатами в форматах x;y, x\ty или x y"
-  (try
-    (let [[a b] (-> s str/trim
-                    (str/replace ";" " ")
-                    (str/replace "\t" " ")
-                    (str/split #"\s+"))]
-      [(Double/parseDouble a) (Double/parseDouble b)])
-    (catch Exception _ nil)))
+  (let [s (-> s str/trim (str/replace #"[;\t]" " "))
+        p (str/split s #"\s+")]
+    (when (= 2 (count p))
+      (try
+        [(Double/parseDouble (p 0))
+         (Double/parseDouble (p 1))]
+        (catch Exception _ nil)))))
 
-(defn is-sorted? [buffer new-point]
-  "Проверяет, что новая точка не нарушает сортировку по x"
-  (if (empty? buffer)
-    true
-    (let [last-x (first (last buffer))
-          new-x (first new-point)]
-      (>= new-x last-x))))
+(defn xs-between [a b step last-x]
+  (let [start (if last-x (max a (+ last-x step)) a)]
+    (take-while #(<= % b)
+                (iterate #(+ % step) start))))
 
-(defn gen-xs [start end step last-x]
-  "Генерирует последовательность точек x с заданным шагом"
-  (let [start (if last-x
-                (max start (+ last-x step))
-                start)]
-    (when (< start end)
-      (->> (iterate #(+ % step) start)
-           (take-while #(<= % end))
-           vec))))
+(defn valid-x? [buffer [x _]]                
+  (or (empty? buffer)
+      (>= x (first (last buffer)))))
 
 ;; Линейная интерполяция
 
-(defn linear-interp [[x1 y1] [x2 y2] x]
-  "Вычисляет значение y для точки x между двумя заданными точками"
+(defn linear-interp [[[x1 y1] [x2 y2]] x]
   (if (= x1 x2)
     y1
-    (+ y1 (* (- y2 y1) (/ (- x x1) (- x2 x1))))))
+    (+ y1 (* (- y2 y1)
+             (/ (- x x1) (- x2 x1))))))
 
-(defn linear-range [window]
-  "Определяет диапазон для линейной интерполяции (последние две точки)"
-  (let [[p1 p2] (take-last 2 window)]
-    [(first p1) (first p2)]))
+(defn process-linear [buffer step last-x]
+  (when (>= (count buffer) 2)
+    (let [[p1 p2] (take-last 2 buffer)
+          [x1 _] p1
+          [x2 _] p2
+          xs (xs-between x1 x2 step last-x)]
+      (when (seq xs)
+        {:last-x (last xs)
+         :out (map (fn [x] [x (linear-interp [p1 p2] x)]) xs)}))))
 
-(defn compute-linear-points [window step last-x]
-  "Вычисляет точки для линейной интерполяции"
-  (let [[x1 x2] (linear-range window)]
-    (gen-xs x1 x2 step last-x)))
-
-(defn create-linear-algorithm []
-  "Создает алгоритм линейной интерполяции"
-  {:window-size (constantly 2)
-   :can-process? #(>= (count %) 2)
-   :get-window #(vec (take-last 2 %))
-   :cleanup-buffer (fn [buffer last-x]
-                     "Удаляет уже обработанные точки, оставляя только последнюю точку окна"
-                     (if last-x
-                       (let [window (take-last 2 buffer)
-                             x2 (first (second window))]
-                         (if (>= last-x x2)
-                           (vec (drop-while #(< (first %) x2) buffer))
-                           buffer))
-                       buffer))
-   :process (fn [_ window step last-x]
-              (let [xs (compute-linear-points window step last-x)]
-                (when (seq xs)
-                  {:pts (mapv (fn [x] [x (linear-interp (first window) (second window) x)]) xs)
-                   :last-x (last xs)})))
-   :process-final (fn [_ window step last-x]
-                    (let [[x1 y1] (first window)
-                          [x2 y2] (second window)
-                          start-x (if last-x
-                                    (max x1 (+ last-x step))
-                                    x1)]
-                      (if (>= start-x x2)
-                        [[x2 y2]]
-                        (let [intermediate-xs (->> (iterate #(+ % step) start-x)
-                                                   (take-while #(< % x2))
-                                                   vec)
-                              all-xs (vec (concat intermediate-xs [x2]))]
-                          (mapv (fn [x] [x (linear-interp [x1 y1] [x2 y2] x)]) all-xs)))))
-   :algorithm-name (constantly "linear")})
-
-;; Интерполяция методом Ньютона
+;; Интерполяция Ньютона
 
 (defn divided-diffs [pts]
-  "Вычисляет разделенные разности для набора точек"
-  (let [xs (map first pts)
-        ys (map second pts)]
-    (loop [level 0 acc [(vec ys)]]
-      (if (= level (dec (count pts)))
+  (let [xs (mapv first pts)]
+    (loop [k 1
+           acc [(mapv second pts)]]
+      (if (= k (count pts))
         acc
-        (recur (inc level)
-               (conj acc
-                     (vec (map (fn [a b x1 x0] (/ (- b a) (- x1 x0)))
-                               (butlast (last acc))
-                               (rest    (last acc))
-                               (drop (inc level) xs)
-                               (drop       level  xs)))))))))
+        (recur
+          (inc k)
+          (conj acc
+                (mapv
+                  (fn [i]
+                    (/ (- (get-in acc [(dec k) (inc i)])
+                          (get-in acc [(dec k) i]))
+                       (- (xs (+ i k))
+                          (xs i))))
+                  (range (- (count pts) k)))))))))
 
-(defn newton-interp [pts x]
-  "Вычисляет значение полинома Ньютона для точки x"
-  (let [xs (map first pts)
+(defn newton-fn [pts]
+  (let [xs  (mapv first pts)
         dif (divided-diffs pts)]
-    (loop [i 1 acc (ffirst dif) w 1.0]
-      (if (= i (count pts))
-        acc
-        (let [w' (* w (- x (nth xs (dec i))))
-              acc' (+ acc (* (nth (nth dif i) 0) w'))]
-          (recur (inc i) acc' w'))))))
+    (fn [x]
+      (reduce
+        (fn [acc i]
+          (+ (get-in dif [i 0])
+             (* (- x (xs i)) acc)))
+        (last (map first dif))
+        (range (dec (count pts)) -1 -1)))))
 
-(defn newton-range [window]
-  "Определяет диапазон для интерполяции Ньютона (от первой до последней точки)"
-  [(ffirst window) (first (last window))])
+(defn process-newton [buffer n step last-x]
+  ;; считаем только при наличии (n + 1)-й точки
+  (when (>= (count buffer) (inc n))
+    (let [window (take n (butlast buffer))
+          [a _] (first window)
+          [b _] (last window)
+          f  (newton-fn window)
+          xs (xs-between a b step last-x)]
+      (when (seq xs)
+        {:last-x (last xs)
+         :out (map (fn [x] [x (f x)]) xs)}))))
 
-(defn compute-newton-points [window step last-x]
-  "Вычисляет точки для интерполяции Ньютона"
-  (let [[a b] (newton-range window)
-        effective-last-x (if (and last-x (>= last-x a) (<= last-x b))
-                           last-x
-                           nil)]
-    (gen-xs a b step effective-last-x)))
-
-(defn create-newton-algorithm [n]
-  "Создает алгоритм интерполяции Ньютона"
-  {:window-size (constantly n)
-   :can-process? #(>= (count %) (inc n))
-   :get-window #(vec (take-last n (butlast %)))
-   :cleanup-buffer (fn [buffer last-x]
-                     "Удаляет уже обработанные точки, сохраняя минимум n+1 точек для работы алгоритма"
-                     (if last-x
-                       (let [min-required (inc n)
-                             buffer-count (count buffer)]
-                         (if (>= buffer-count min-required)
-                           (let [window (take-last n (butlast buffer))
-                                 window-start-x (first (first window))
-                                 window-end-x (first (last window))]
-                             (if (>= last-x window-end-x)
-                               (let [cleaned (vec (drop-while #(< (first %) window-start-x) buffer))]
-                                 (if (>= (count cleaned) min-required)
-                                   cleaned
-                                   (vec (take-last min-required buffer))))
-                               buffer))
-                           buffer))
-                       buffer))
-   :process (fn [_ window step last-x]
-              (let [xs (compute-newton-points window step last-x)]
-                (when (seq xs)
-                  {:pts (mapv (fn [x] [x (newton-interp window x)]) xs)
-                   :last-x (last xs)})))
-   :process-final (fn [_ window step last-x]
-                    (let [[x-start] (newton-range window)
-                          x-end (first (last window))
-                          start-x (if last-x
-                                    (max x-start (+ last-x step))
-                                    x-start)]
-                      (if (>= start-x x-end)
-                        [[x-end (newton-interp window x-end)]]
-                        (let [intermediate-xs (->> (iterate #(+ % step) start-x)
-                                                   (take-while #(< % x-end))
-                                                   vec)
-                              all-xs (vec (concat intermediate-xs [x-end]))]
-                          (mapv (fn [x] [x (newton-interp window x)]) all-xs)))))
-   :algorithm-name (constantly "newton")})
-
-;; Обработка потока данных
-
-(defn process-algorithm
-  "Обрабатывает один алгоритм интерполяции для текущего буфера.
-   Возвращает {:last-x x :cleaned-buffer buffer} или nil"
-  [algorithm buffer step last-x]
-  (when ((:can-process? algorithm) buffer)
-    (let [window ((:get-window algorithm) buffer)
-          result ((:process algorithm) algorithm window step last-x)]
-      (when result
-        (doseq [[x y] (:pts result)]
-          (println (str ((:algorithm-name algorithm)) ":") x y))
-        (let [new-last-x (:last-x result)
-              cleaned-buffer ((:cleanup-buffer algorithm) buffer new-last-x)]
-          {:last-x new-last-x
-           :cleaned-buffer cleaned-buffer})))))
-
-(defn process-algorithms-final
-  "Финальная обработка всех алгоритмов при EOF"
-  [algorithms buffer step last-states]
-  (doseq [algorithm algorithms]
-    (let [algo-name ((:algorithm-name algorithm))
-          window-size ((:window-size algorithm))]
-      (when (>= (count buffer) window-size)
-        (let [window (if (= algo-name "newton")
-                       (vec (take-last window-size buffer))
-                       ((:get-window algorithm) buffer))
-              last-x (get last-states algo-name)
-              result ((:process-final algorithm) algorithm window step last-x)]
-          (doseq [[x y] result]
-            (println (str algo-name ":") x y)))))))
-
-(defn process-stream
-  "Основная функция обработки потока данных"
-  [algorithms step]
+(defn process-stream [use-linear? use-newton? n step]
   (loop [buffer []
-         last-states {}]
+         last-linear-x nil
+         last-newton-x nil]
     (if-let [line (read-line)]
       (if-let [pt (parse-line line)]
-        (if (is-sorted? buffer pt)
-          (let [new-buffer (conj buffer pt)
-                {updated-states :states
-                 results :results}
-                (reduce (fn [acc algorithm]
-                          (let [algo-name ((:algorithm-name algorithm))
-                                result (process-algorithm algorithm new-buffer step (get (:states acc) algo-name))]
-                            (if result
-                              {:states (assoc (:states acc) algo-name (:last-x result))
-                               :results (conj (:results acc) result)}
-                              {:states (:states acc)
-                               :results (:results acc)})))
-                        {:states last-states :results []}
-                        algorithms)
-                cleaned-buffer (if (seq results)
-                                 (->> results
-                                      (map :cleaned-buffer)
-                                      (reduce (fn [min-buf buf]
-                                                (if (< (count buf) (count min-buf))
-                                                  buf
-                                                  min-buf))
-                                              new-buffer))
-                                 new-buffer)]
-            (recur cleaned-buffer updated-states))
-          ;; Точка нарушает сортировку - выводим сообщение и пропускаем
+        (if (valid-x? buffer pt)                    
+          (let [buffer' (conj buffer pt)
+
+                linear-res (when use-linear?
+                             (process-linear buffer' step last-linear-x))
+
+                newton-res (when use-newton?
+                             (process-newton buffer' n step last-newton-x))]
+
+            ;; вывод
+            (when linear-res
+              (doseq [[x y] (:out linear-res)]
+                (println "linear :" x y)))
+
+            (when newton-res
+              (doseq [[x y] (:out newton-res)]
+                (println "newton :" x y)))
+
+            ;; ограничиваем буфер
+            (let [max-size (max 2 (inc n))
+                  buffer'' (vec (take-last max-size buffer'))]
+              (recur buffer''
+                     (or (:last-x linear-res) last-linear-x)
+                     (or (:last-x newton-res) last-newton-x))))
+
+          ;; x нарушает сортировку                  
           (do
-            (println "ERROR: Point violates x sorting order. It'll be skipped.")
-            (recur buffer last-states)))
-        ;; Строка не распарсилась
-        (recur buffer last-states))
+            (binding [*out* *err*]
+              (println "ERROR: x must be non-decreasing, skipped:" pt))
+            (recur buffer last-linear-x last-newton-x)))
+
+        ;; строка не распарсилась
+        (recur buffer last-linear-x last-newton-x))
+
+      ;; EOF
       (do
-        (process-algorithms-final algorithms buffer step last-states)
+        (when use-linear?
+          (when-let [res (process-linear buffer step last-linear-x)]
+            (doseq [[x y] (:out res)]
+              (println "linear :" x y))))
+
+        (when use-newton?
+          (when-let [res (process-newton buffer n step last-newton-x)]
+            (doseq [[x y] (:out res)]
+              (println "newton :" x y))))
+
         (System/exit 0)))))
 
-;; Парсинг аргументов командной строки
 
 (defn parse-args [args]
-  "Парсит аргументы командной строки и возвращает конфигурацию"
-  (let [algos (->> args
-                   (filter #(#{"--linear" "--newton"} %))
-                   (map {"--linear" :linear "--newton" :newton})
-                   (into []))
-        algos (if (empty? algos) [:linear] algos)
-
-        step-idx (.indexOf args "--step")
-        step (if (and (>= step-idx 0) (< (inc step-idx) (count args)))
-               (Double/parseDouble (nth args (inc step-idx)))
-               1.0)
-
-        n-idx (.indexOf args "-n")
-        n (if (and (>= n-idx 0) (< (inc n-idx) (count args)))
-            (Integer/parseInt (nth args (inc n-idx)))
-            4)]
-    {:algorithms algos
-     :step step
-     :n n}))
-
-(defn create-algorithms [config]
-  "Создает список алгоритмов интерполяции на основе конфигурации"
-  (let [{:keys [algorithms n]} config]
-    (mapv (fn [algo-type]
-            (case algo-type
-              :linear (create-linear-algorithm)
-              :newton (create-newton-algorithm n)))
-          algorithms)))
+  {:linear? (some #{"--linear"} args)
+   :newton? (some #{"--newton"} args)
+   :step    (or (some->> args
+                         (drop-while #(not= % "--step"))
+                         second
+                         Double/parseDouble)
+                1.0)
+   :n       (or (some->> args
+                         (drop-while #(not= % "-n"))
+                         second
+                         Integer/parseInt)
+                4)})
 
 (defn -main [& args]
-  (let [config (parse-args args)
-        algorithms (create-algorithms config)
-        step (:step config)]
-    (process-stream algorithms step)))
-
+  (let [{:keys [linear? newton? n step]} (parse-args args)
+        linear? (or linear? (not newton?))]
+    (process-stream linear? newton? n step)))
--- lab3/interpolation/test/interpolation/core_test.clj
+++ lab3/interpolation/test/interpolation/core_test.clj
@@ -2,94 +2,81 @@
   (:require [clojure.test :refer :all]
             [interpolation.core :refer :all]))
 
-(deftest test-linear-interp
-  (testing "Линейная интерполяция между двумя точками"
-    (is (= 0.5 (linear-interp [0 0] [1 1] 0.5)))
-    (is (= 1.5 (linear-interp [1 1] [2 2] 1.5)))
-    (is (= 0.0 (linear-interp [0 0] [1 1] 0.0)))
-    (is (= 1.0 (linear-interp [0 0] [1 1] 1.0)))))
-
-(deftest test-newton-interp
-  (testing "Интерполяция Ньютона для набора точек"
-    (let [points [[0 0] [1 1] [2 4] [3 9]]]
-      ;; Проверяем, что интерполяция возвращает значения для известных точек
-      (is (= 0.0 (newton-interp points 0.0)))
-      (is (= 1.0 (newton-interp points 1.0)))
-      ;; Проверяем, что функция возвращает число
-      (let [result (newton-interp points 1.5)]
-        (is (number? result))
-        (is (not (Double/isNaN result)))))))
 
 (deftest test-parse-line
-  (testing "Парсинг строк с координатами"
+  (testing "Корректный парсинг входных строк"
     (is (= [0.0 0.0] (parse-line "0 0")))
     (is (= [1.0 2.0] (parse-line "1 2")))
     (is (= [1.5 2.5] (parse-line "1.5 2.5")))
     (is (= [1.0 2.0] (parse-line "1;2")))
     (is (= [1.0 2.0] (parse-line "1\t2")))
-    (is (nil? (parse-line "invalid")))))
+    (is (nil? (parse-line "invalid")))
+    (is (nil? (parse-line "1 2 3")))))
 
-(deftest test-is-sorted?
-  (testing "Проверка сортировки точек"
-    (is (true? (is-sorted? [] [1 1])))
-    (is (true? (is-sorted? [[0 0]] [1 1])))
-    (is (true? (is-sorted? [[0 0]] [0 1])))
-    (is (false? (is-sorted? [[1 1]] [0 0])))))
+(deftest test-valid-x
+  (testing "Проверка монотонности x"
+    (is (true? (valid-x? [] [0 0])))
+    (is (true? (valid-x? [[0 0]] [1 1])))
+    (is (true? (valid-x? [[0 0]] [0 1])))
+    (is (false? (valid-x? [[1 1]] [0 0])))))
 
-(deftest test-gen-xs
-  (testing "Генерация последовательности точек x"
-    (is (= [0 1.0 2.0] (gen-xs 0 2 1.0 nil)))
-    (is (= [1.5 2.0] (gen-xs 0 2 0.5 1.0)))
-    (is (nil? (gen-xs 2 1 1.0 nil)))))
+(deftest test-xs-between
+  (testing "Генерация x с учетом шага и last-x"
+    (is (= [0.0 1.0 2.0]
+           (take 3 (xs-between 0.0 2.0 1.0 nil))))
+    (is (= [1.5 2.0]
+           (take 2 (xs-between 0.0 2.0 0.5 1.0))))))
 
-(deftest test-linear-algorithm
-  (testing "Алгоритм линейной интерполяции"
-    (let [algorithm (create-linear-algorithm)]
-      (is (= 2 ((:window-size algorithm))))
-      (is (= "linear" ((:algorithm-name algorithm))))
-      (is (true? ((:can-process? algorithm) [[0 0] [1 1]])))
-      (is (false? ((:can-process? algorithm) [[0 0]])))
-      (is (= [[1 1] [2 2]] ((:get-window algorithm) [[0 0] [1 1] [2 2]])))
-      (let [result ((:process algorithm) algorithm [[0 0] [1 1]] 0.5 nil)]
-        (is (some? result))
-        (is (some? (:last-x result)))
-        (is (pos? (count (:pts result))))))))
+(deftest test-linear-interp
+  (testing "Линейная интерполяция между двумя точками"
+    (let [p1 [0.0 0.0]
+          p2 [1.0 1.0]]
+      (is (= 0.0 (linear-interp [p1 p2] 0.0)))
+      (is (= 0.5 (linear-interp [p1 p2] 0.5)))
+      (is (= 1.0 (linear-interp [p1 p2] 1.0))))))
 
-(deftest test-newton-algorithm
-  (testing "Алгоритм интерполяции Ньютона"
-    (let [algorithm (create-newton-algorithm 4)]
-      (is (= 4 ((:window-size algorithm))))
-      (is (= "newton" ((:algorithm-name algorithm))))
-      (is (true? ((:can-process? algorithm) [[0 0] [1 1] [2 2] [3 3] [4 4]])))
-      (is (false? ((:can-process? algorithm) [[0 0] [1 1] [2 2] [3 3]])))
-      (is (= [[1 1] [2 2] [3 3] [4 4]] ((:get-window algorithm) [[0 0] [1 1] [2 2] [3 3] [4 4] [5 5]])))
-      (let [result ((:process algorithm) algorithm [[0 0] [1 1] [2 4] [3 9]] 0.5 nil)]
-        (is (some? result))
-        (is (some? (:last-x result)))
-        (is (pos? (count (:pts result))))))))
-
-(deftest test-compute-linear-points
-  (testing "Генерация точек для линейной интерполяции"
-    (let [window [[0 0] [1 1]]
-          step 0.5]
-      (is (vector? (compute-linear-points window step nil)))
-      (is (pos? (count (compute-linear-points window step nil))))
-      (is (= [0.5 1.0] (compute-linear-points window step 0.0))))))
+(deftest test-process-linear
+  (testing "Потоковая линейная интерполяция"
+    (let [buffer [[0 0] [1 1]]
+          step 0.5
+          res (process-linear buffer step nil)]
+      (is (some? res))
+      (is (= 1.0 (:last-x res)))
+      (is (every? true?
+                  (map (fn [[x y] [ex ey]]
+                         (and (== x ex) (== y ey)))
+                       (:out res)
+                       [[0.0 0.0] [0.5 0.5] [1.0 1.0]]))))))
 
 (deftest test-divided-diffs
-  (testing "Вычисление разделенных разностей"
-    (let [points [[0 0] [1 1] [2 4]]
-          diffs (divided-diffs points)]
-      (is (vector? diffs))
+  (testing "Разделённые разности Ньютона"
+    (let [pts [[0 0] [1 1] [2 4]]
+          diffs (divided-diffs pts)]
       (is (= 3 (count diffs)))
       (is (= [0 1 4] (first diffs))))))
 
-(deftest test-linear-range
-  (testing "Определение диапазона для линейной интерполяции"
-    (let [window [[0 0] [1 1] [2 2]]]
-      (is (= [1 2] (linear-range window))))))
+(deftest test-newton-fn
+  (testing "Полином Ньютона проходит через заданные точки"
+    (let [pts [[0 0] [1 1] [2 4] [3 9]]
+          f   (newton-fn pts)]
+      (is (= 0.0 (f 0.0)))
+      (is (= 1.0 (f 1.0)))
+      (is (= 4.0 (f 2.0)))
+      (is (= 9.0 (f 3.0)))
+      (is (number? (f 1.5))))))
 
-(deftest test-newton-range
-  (testing "Определение диапазона для интерполяции Ньютона"
-    (let [window [[0 0] [1 1] [2 4] [3 9]]]
-      (is (= [0 3] (newton-range window))))))
+(deftest test-process-newton
+  (testing "Потоковая интерполяция Ньютона (n точек, триггер n+1)"
+    (let [buffer [[0 0] [1 1] [2 4] [3 9] [4 16]]
+          n 4
+          step 0.5
+          res (process-newton buffer n step nil)]
+      (is (some? res))
+      (is (= 3.0 (:last-x res)))
+      (is (some #(= [1.0 1.0] %) (:out res)))
+      (is (some #(= [2.0 4.0] %) (:out res))))))
+
+(deftest test-insufficient-data
+  (testing "Недостаточно данных для интерполяции"
+    (is (nil? (process-linear [[0 0]] 0.5 nil)))
+    (is (nil? (process-newton [[0 0] [1 1] [2 2]] 4 0.5 nil)))))
